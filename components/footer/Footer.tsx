f42fa5328612ef2650d07af4c0b4dfa77862fbb5
sharmaketan14 <ks456167@gmail.com>
2023-11-15 22:09:08 +0530

    About Page First Draft Done

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -7,33 +7,34 @@ import BottomLine from "../../public/footer/Bottom Line.png"
 import { Poppins, Roboto } from "next/font/google";
 import { useState } from "react";
 import Twitter from "./tweet";
+import Link from "next/link";
 
 interface Icon {
-    show : boolean
+    show: boolean
 }
-const initialState = {show : true};
-const finalState = {show : true};
+const initialState = { show: true };
+const finalState = { show: true };
 const Footer = () => {
     const [place_icon, setPlace_icon] = useState<Icon>(initialState);
-    
+
     const handleChange = (e: any) => {
         if (e.target.value !== "") {
-            setPlace_icon({ show: false});
+            setPlace_icon({ show: false });
         } else {
-            setPlace_icon({ show: true});
+            setPlace_icon({ show: true });
         }
     }
     return (
         <div className="flex flex-col bg-black">
             <div className="h-300px flex flex-col md:flex-row justify-center items-center bg-black mb-10">
                 <div className="w-45 m-5 pb-10 flex flex-col">
-                    <Image className="mx-5" src={Logo} width={150} height={150} alt="Cisco Community Logo"/>
+                    <Image className="mx-5" src={Logo} width={150} height={150} alt="Cisco Community Logo" />
                     <h1 className="mx-5 mt-6 w-22 text-white font-Poppins text-3xl font-bold">Get your career&apos;s inital</h1>
                     <h1 className="mx-5 mb-7 w-22 text-white font-Poppins text-3xl font-bold">boost with our community.</h1>
                     <form className="mx-5">
                         {
                             place_icon.show === true ? <svg className="mx-4 mt-4 absolute bottom-100 z-100 w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z" />
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z" />
                             </svg>
                                 : null
                         }
@@ -46,14 +47,30 @@ const Footer = () => {
                 </div>
             </div>
             <ul className="mx-5 mb-10 flex flex-col md:flex-row xl:mx-40 justify-between text-white font-Poppins">
-                <li className="mx-5">Features</li>
-                <li className="mx-5">Team Members</li>
-                <li className="mx-5">News & Blogs</li>
-                <li className="mx-5">Careers</li>
-                <li className="mx-5">About Us</li>
-                <li className="mx-5">Terms</li>
-                <li className="mx-5">Privacy</li>
-                <li className="mx-5">Cookies</li>
+                <li className="mx-5">
+                    <Link href={"#"}>Features</Link>
+                </li>
+                <li className="mx-5">
+                    <Link href={"#"}>Team Members</Link>
+                </li>
+                <li className="mx-5">
+                    <Link href={"#"}>News & Blogs</Link>
+                </li>
+                <li className="mx-5">
+                    <Link href={"#"}>Careers</Link>
+                </li>
+                <li className="mx-5">
+                    <Link href={"/about"}>About Us</Link>
+                </li>
+                <li className="mx-5">
+                    <Link href={"#"}>Terms</Link>
+                </li>
+                <li className="mx-5">
+                    <Link href={"#"}>Privacy</Link>
+                </li>
+                <li className="mx-5">
+                    <Link href={"#"}>Cookies</Link>
+                </li>
             </ul>
             <Image alt="Bottom Line" src={BottomLine} width={1800} />
         </div>
