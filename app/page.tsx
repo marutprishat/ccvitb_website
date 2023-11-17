477c4b30370ddcee36f309a1812f760deb7e45c5
sharmaketan14 <ks456167@gmail.com>
2023-11-17 13:57:03 +0530

    Implemented Timeline

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutUs from "./AboutUs";
 import Contact from "./Contact";
 import TheTeam from "./TheTeam";
+import Timeline from "../components/timeline";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col pl-5 pr-5">
       <AboutUs/>
+      <Timeline/>
       <TheTeam/>
       <Contact/>
     </main>
