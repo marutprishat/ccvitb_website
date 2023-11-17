d3998003dd28b076586afbbd5ebfe2f7c6bcacac
Rishav <111266613+rissshaaav@users.noreply.github.com>
2023-11-17 13:59:29 +0530

    adjustments in Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col pl-5 pr-5">
       <AboutUs/>
+      <div id="Timeline"></div>
       <TheTeam/>
       <Contact/>
     </main>
