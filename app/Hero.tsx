06a7a085d2ba7d5c8cd34817e625267f8ced2ca7
Prakhar <prakharswarnkar29@gmail.com>
2023-11-17 17:16:32 +0530

    Pushed Home Section

---
+++ app/Hero.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+import Button from '@/components/Button'
+
+const Hero = () => {
+  return (
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+     <div className="hero-rocket" />
+     <div className="adjusted-hero-astro" />
+
+
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <Image 
+          src="/rocket.png"
+          alt="rocket"
+          width={50}
+          height={50}
+          className="absolute left-[-18px] top-[-80px] w-10 lg:w-[130px] "
+        />
+        <h1 className="bold-52 lg:bold-87" style={{ color: 'rgb(110, 190, 74)' }}>DIVE INTO THE WONDERS OF THE CONNECTICITY</h1>
+        
+        <p className="regular-16 mt-6 text-black xl:max-w-[520px]">
+        Embark on a tech odyssey at Cisco Community, VIT Bhopal. Explore Networking Wonders, connect with experts, and shape the future. Join us in shaping tomorrow's connectivity! 
+        </p>
+
+        <div className="my-5 flex flex-wrap gap-3">
+  
+
+        
+        </div>
+
+        <div className="flex flex-col w-full gap-3 sm:flex-row">
+          <Button 
+            type="button" 
+            title="Browse Community" 
+            variant="btn_blue" 
+          />
+          
+        </div>
+      </div>
+
+      <div className="relative flex flex-1 items-start">
+        
+      </div>
+    </section>
+  )
+}
+
+export default Hero
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import About from "./About";
 import Contact from "./Contact";
 import TheTeam from "./TheTeam";
+import Hero from "./Hero";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
+      <Hero/>
       <About/>
       <TheTeam/>
       <Contact/>
