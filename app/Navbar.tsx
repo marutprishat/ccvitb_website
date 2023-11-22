8c769a12b91b8d8c34c4e41a9a72acb7f8b18101
Rishav <111266613+rissshaaav@users.noreply.github.com>
2023-11-22 13:27:04 +0530

    modified navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,74 +1,159 @@
-'use client';
+import Image from "next/image"
+import Link from "next/link"
 
-import { useState, useEffect } from "react";
-import MenuButton from "@/components/MenuButton";
-import Image from "next/image";
-import Link from "next/link";
-
-const Navbar = () => {
-    const [activeSection, setActiveSection] = useState('');
-    
-
-    const handleScroll = () => {
-        // const homeOffset = document.getElementById('Home').offsetTop;
-        const aboutOffset = document.getElementById('About').offsetTop;
-        const timelineOffset = document.getElementById('Timeline').offsetTop;
-        const teamOffset = document.getElementById('Team').offsetTop;
-        const contactOffset = document.getElementById('Contact').offsetTop;
-
-        const scrollPosition = window.scrollY;
-
-        if (scrollPosition < aboutOffset) {
-            setActiveSection('home');
-        } else if (scrollPosition >= aboutOffset && scrollPosition < timelineOffset) {
-            setActiveSection('about');
-        } else if (scrollPosition >= timelineOffset && scrollPosition < teamOffset) {
-            setActiveSection('timeline');
-        } else if (scrollPosition >= teamOffset && scrollPosition < contactOffset) {
-            setActiveSection('team');
-        } else {
-            setActiveSection('contact');
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+const Navbar3 = () => {
     return (
-        <div className="sticky top-0 w-full px-2 sm:px-8 py-2 sm:py-4 z-50 bg-gradient-to-r from-[#6ebf49] to-[#00bcea] to-[70%] sm:to-[50%]">
-            <div className="w-full flex items-center justify-between gap-x-10 backdrop-blur-md">
-                <div className="w-[120px] h-[50px] sm:w-[176px] sm:h-[50px]">
+        <div className="fixed top-0 left-0 z-10 navbar bg-base-100 bg-gradient-to-r from-[#00bcea] to-[#6ebf49]">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={1} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {/* <li><a>Item 1</a></li> */}
+                        <li>
+                            <a>Sections</a>
+                            <ul className="p-2">
+                                <li><Link href='#Home'>Home</Link></li>
+                                <li><Link href='#About'>About</Link></li>
+                                <li><Link href='#Timeline'>Timeline</Link></li>
+                                <li><Link href='#Team'>Team</Link></li>
+                                <li><Link href='#Contact'>Contact</Link></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a>Social Media</a>
+                            <ul className="p-2">
+                                <li>
+                                    <a href='https://www.instagram.com/ciscovitb/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA==' target="_blank">
+                                        <Image
+                                            src='/icons/instagram.svg'
+                                            alt="insta"
+                                            height={20}
+                                            width={20}
+                                        />
+                                    </a>
+                                </li>
+                                <li>
+                                    <Link href="https://x.com/CiscoVitB?s=20" target="_blank">
+                                        <Image
+                                            src='/icons/twitter.svg'
+                                            alt="insta"
+                                            height={20}
+                                            width={20}
+                                        />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href='#' target="_blank">
+                                        <Image
+                                            src='/icons/facebook.svg'
+                                            alt="insta"
+                                            height={20}
+                                            width={20}
+                                        />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href='#' target="_blank">
+                                        <Image
+                                            src='/icons/youtube.svg'
+                                            alt="insta"
+                                            height={20}
+                                            width={20}
+                                        />
+                                    </Link>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a>Community</a></li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">
                     <Image
-                        className="object-contain"
-                        alt="Image"
-                        src="/images/CCVITBLogo.png"
-                        width={120}
-                        height={50}
+                        className=""
+                        src='/images/CCVITBLogo.png'
+                        alt="CCVITB logo"
+                        width={90}
+                        height={45}
                     />
-                </div>
-                {/* <Image
-                    className="hidden sm:block object-contain"
-                    alt="Line"
-                    src="/line-13.png"
-                    width={1}
-                    height={41}
-                /> */}
-                <div className="hidden sm:flex items-center gap-x-8  [font-family:'Outfit-Regular',Helvetica] font-normal text-[#002b3d] text-[21px] text-center tracking-[0] leading-[normal]">
-                    <div className={activeSection === 'home' ? 'opacity-[100%]' : 'opacity-50'}><Link href="" >Home</Link></div>
-                    <div className={activeSection === 'about' ? 'opopacity-[100%]' : 'opacity-50'}><Link href="#About" >About Us</Link></div>
-                    <div className={activeSection === 'timeline' ? 'opacity-[100%]' : 'opacity-50'}><Link href="#Timeline" >Timeline</Link></div>
-                    <div className={activeSection === 'team' ? 'opacity-[100%]' : 'opacity-50'}><Link href="#Team" >Our Team</Link></div>
-                    <div className={activeSection === 'contact' ? 'opacity-[100%]' : 'opacity-50'}><Link href="#Contact" >Contact</Link></div>
-                </div>
-                <div className="block sm:hidden">
-                    <MenuButton />
-                </div>
+                </a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {/* <li><a>Item 1</a></li> */}
+                    <li tabIndex={0}>
+                        <details>
+                            <summary>Sections</summary>
+                            <ul className="p-2">
+                                <li><Link href='#Home'>Home</Link></li>
+                                <li><Link href='#About'>About</Link></li>
+                                <li><Link href='#Timeline'>Timeline</Link></li>
+                                <li><Link href='#Team'>Team</Link></li>
+                                <li><Link href='#Contact'>Contact</Link></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li tabIndex={1}>
+                        <details>
+                            <summary>Social Media</summary>
+                            <ul className="p-2">
+                                <li>
+                                    <a href='https://www.instagram.com/ciscovitb/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA==' target="_blank">
+                                        <Image
+                                            src='/icons/instagram.svg'
+                                            alt="insta"
+                                            height={20}
+                                            width={20}
+                                        />
+                                    </a>
+                                </li>
+                                <li>
+                                    <Link href="https://x.com/CiscoVitB?s=20" target="_blank">
+                                        <Image
+                                            src='/icons/twitter.svg'
+                                            alt="insta"
+                                            height={20}
+                                            width={20}
+                                        />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href='#' target="_blank">
+                                        <Image
+                                            src='/icons/facebook.svg'
+                                            alt="insta"
+                                            height={20}
+                                            width={20}
+                                        />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href='#' target="_blank">
+                                        <Image
+                                            src='/icons/youtube.svg'
+                                            alt="insta"
+                                            height={20}
+                                            width={20}
+                                        />
+                                    </Link>
+                                </li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Community</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                {/* <a className="btn">Button</a> */}
+                <label className="cursor-pointer grid place-items-center">
+                    <input type="checkbox" value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
+                    <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+                    <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                </label>
             </div>
         </div>
     )
 }
 
-export default Navbar
+export default Navbar3
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Footer from '@/components/footer/Footer'
 import './globals.css'
 import Navbar from './Navbar';
+import Navbar3 from './Navbar';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        <Navbar3/>
         {children}
         <Footer/>
       </body>
