a6041613569dba25d9fd67e311fc31f43d008340
Rishav <111266613+rissshaaav@users.noreply.github.com>
2023-12-08 08:52:26 +0530

    updated teams section

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -28,10 +28,10 @@ const Footer = () => {
         <div className="flex flex-col bg-black">
             <div className="h-300px flex flex-col md:flex-row justify-center items-center bg-black mb-10">
                 <div className="w-45 m-5 pb-10 flex flex-col">
-                    <Image className="mx-5" src={Logo} width={150} height={150} alt="Cisco Community Logo" />
-                    <h1 className="mx-5 mt-6 w-22 text-white font-Poppins text-3xl font-bold">Get your career&apos;s initial</h1>
-                    <h1 className="mx-5 mb-7 w-22 text-white font-Poppins text-3xl font-bold">boost with our community.</h1>
-                    <form className="mx-5">
+                    <Image className="mx-5" src={Logo} width={250} height={150} alt="Cisco Community Logo" />
+                    <h1 className="mx-5 mt-6 w-22 text-white font-Poppins text-3xl font-semibold">Get your career&apos;s initial</h1>
+                    <h1 className="mx-5 mb-7 w-22 text-white font-Poppins text-3xl font-semibold">boost with our community.</h1>
+                    {/* <form className="mx-5">
                         {
                             place_icon.show === true ? <svg className="mx-4 mt-4 absolute bottom-100 z-100 w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 18">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z" />
@@ -40,7 +40,7 @@ const Footer = () => {
                         }
                         <input type="email" placeholder="Enter your email" className="h-10 pl-10 w-80 bg-black border rounded-3xl border-white font-Poppins font-thin text-white" onChange={handleChange} />
                         <button className="sm:mx-5 my-2 mx-2 px-5 h-10 rounded-3xl font-Poppins font-bold text-white text-xs bg-gradient-to-b from-gray-700 via-stone-600 to-lime-600" type="submit">Subscribe</button>
-                    </form>
+                    </form> */}
                 </div>
                 <div className="mx-5">
                     <Twitter />
